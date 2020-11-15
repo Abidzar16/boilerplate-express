2f017523925726216b608121333b339bf132adb2
Abidzar16 <abidzarisprivate@gmail.com>
2020-11-15 11:34:23 +0000

    add .env file

--- myApp.js
+++ myApp.js
@@ -1,39 +1,31 @@
 var express = require('express');
 var app = express();
+var response = "Hello World";
 
+if (process.env.MESSAGE_STYLE === "uppercase") {
+  response = "Hello World".toUpperCase();
+} else {
+  response = "Hello World";
+}
 
+absolutePath = __dirname + "/views/index.html"
 
+// Normal usage
+app.use(express.static(__dirname + "/public"));
 
+app.get("/json", (req, res) => {
+  res.json({
+    "message": response
+  });
+});
 
+// Assets at the /assets route
+app.use("/assets", express.static(__dirname + "/public"));
 
+app.use(function(req, res) {
+  res.sendFile(absolutePath);
+});
 
+console.log("hello world")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
